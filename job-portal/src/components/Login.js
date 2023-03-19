96e1046dc8884d404476ee9f35599ff47fdf2c47
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-03-19 23:06:13 +0530

    added regirstration

--- job-portal/src/components/Login.js
+++ job-portal/src/components/Login.js
@@ -35,6 +35,49 @@ function Login(){
         .catch(err => console.error("errors : " , err));
     }
 
+    function handleChange(event) {
+        if(event.target.name === "username") {
+            var tempusername = event.target.value;
+            setUsername(tempusername);
+        }
+        else if (event.target.name === 'password') {
+            var temppassword = event.target.value;
+            setPassword(temppassword);
+        }
+
+    }
+
+    const addUsers = async () => {
+        var inusername = document.getElementById('1').value;
+        var userpass = document.getElementById('2').value;
+        var confirmepass = document.getElementById('3').value;
+        console.log(inusername + ' -=== ' + userpass )
+        if(users.some(item => (item.username === inusername))){
+            alert("User Already registered, Please Login");
+           }
+
+        if(userpass == confirmepass){
+             setUsername(inusername);
+             setPassword(userpass);   
+        }else{
+            alert("Wrong passwords");
+            return;
+        }
+        console.log(username+"-"+password);
+        const data = await fetch(API_BASE + "/auth/register",{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({
+                username : username,
+                password : password
+            })
+        }).then(res => res.json());
+
+        console.log(data)
+    }
+
     const [isMouseOver, setMouseOver] = useState(false);
 
     function handleMouseOver() {
@@ -43,20 +86,32 @@ function Login(){
     function handleMouseOut() {
         setMouseOver(false);
     }
-    function func(){
-        console.log(users)
-       var inusername = document.getElementById('1').value;
-        var userpass = document.getElementById('2').value;
-        console.log(inusername + " - " + userpass);
-       if(users.some(item => (item.username === inusername) && (item.password === userpass))){
-        setUsername(inusername);
-        setPassword(userpass);
-        console.log('hogay are')
-       setisLoggedIn(true)
+    function func (){
+        if(isUserRegistered){
+            console.log(users)
+            var inusername = document.getElementById('1').value;
+             var userpass = document.getElementById('2').value;
+             console.log(inusername + " - " + userpass);
+            if(users.some(item => (item.username === inusername) && (item.password === userpass))){
+             setUsername(inusername);
+             setPassword(userpass);
+             console.log('hogay are')
+            setisLoggedIn(true)
+            }else{
+                alert("Username or Password Wrong");
+            }
+        }else{
+           addUsers();  
+
+        }
+        
     }
 
+function timeout(delay) {
+    return new Promise( res => setTimeout(res, delay) );
+}
 
-    }
+    
     return (
         <>
 
@@ -73,10 +128,10 @@ function Login(){
     <form className="form">
 
         {/* {attributes.map(createInputElement)} */}
-        <input type="text" placeholder="Username" id="1"></input>
-        <input type="text" placeholder="Password" id="2"></input>
+        <input type="text" placeholder="Username" name='username' onChange={handleChange} id="1"></input>
+        <input type="text" placeholder="Password" name= 'password' onChange= {handleChange} id="2"></input>
 
-        { !isUserRegistered && < Input type="text" placeholder="Confirm Password" />}
+        { !isUserRegistered && <input type="text" placeholder="Confirm Password" id="3"></input>}
         
     </form>
     <button type="submit"
@@ -90,11 +145,13 @@ function Login(){
 
             
         </button>  
+        {isUserRegistered ? <p>Don't have an account? <a onClick={() => { setisUserRegistered(false)}}>click here</a> to register</p> : <p>Already registered? <a onClick={()=> {setisUserRegistered(true)}}>click here</a> to login</p> }
+      
     </div>
      }
         
-        </>
         
+     </>
     )
 }
 export default Login
