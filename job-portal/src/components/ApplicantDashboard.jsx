c83dd242954f7db7b2153bf77756be9594dc3b49
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-04-13 22:46:05 +0530

    Added Bootstrap

--- job-portal/src/components/ApplicantDashboard.jsx
+++ job-portal/src/components/ApplicantDashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import "../components/assets/AppDash.css";
 import { NavLink } from 'react-router-dom';
 import {Nav, Navbar} from 'react-bootstrap';
