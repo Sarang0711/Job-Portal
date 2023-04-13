681ca31eaed473983a9e18c0ea56551fcd979f57
Sarang0711 <sarangs_c20026@students.isquareit.edu.in>
2023-04-13 14:09:26 +0530

    Edited Navbar

--- job-portal/src/components/ApplicantDashboard.jsx
+++ job-portal/src/components/ApplicantDashboard.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../components/assets/AppDash.css";
-
+import { NavLink } from 'react-router-dom';
+import {Nav, Navbar} from 'react-bootstrap';
+import {BrowserRouter as Router, Routes,  Route} from 'react-router-dom';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import { NavDropdown } from 'react-bootstrap';
 function ApplicantDashboard() {
   return (
     <>
-    <div className="applicant-dash">
+      {/* <Router> */}
+      <Navbar bg="light" className='custom-nav' >
+        <Navbar.Brand href="/">Job Portal</Navbar.Brand>
+        {/* <NavbarToggle aria-controls='basic-navbar-nav'/> */}
+        <NavbarCollapse id='basic-navbar-nav'>
+          <Nav className='mx-auto'>
+            <NavLink exact="true" path="/dashboard" className="nav-link" >New Jobs</NavLink>
+            <NavLink path="/applied" className="nav-link" >Applied</NavLink>
+            <NavLink path="/referfriend" className="nav-link" >Refer a Friend</NavLink>
+            <NavLink path="/messages" className="nav-link" >Messages</NavLink>
+            <div className="profile">
+              <NavDropdown title="" id='profile-dropdown'>
+                <NavDropdown.Item>Edit Resume</NavDropdown.Item>
+                <NavDropdown.Item>Bootmarked</NavDropdown.Item>
+                <NavDropdown.Item>Help</NavDropdown.Item>
+                <NavDropdown.Item>Log Out</NavDropdown.Item>
+              </NavDropdown>
+            </div>
+          </Nav>
+        </NavbarCollapse>
+      </Navbar>
+      {/* </Router> */}
+    </>
+  )
+}
+
+export default ApplicantDashboard;
+
+
+{/* <div className="applicant-dash">
       <div className="app-nav">
         <div className="logo">
           <p><strong>Job Portal</strong></p>
@@ -16,10 +51,4 @@ function ApplicantDashboard() {
           <p>Messages</p>
         </div>
       </div>
-    </div>
-      
-    </>
-  )
-}
-
-export default ApplicantDashboard;
+    </div> */}
