e33ae65a59294e2bb9dbb80d5ddb00c698187d00
VaibhavDhaygude <vaibhavdhaygude70@gmail.com>
2023-04-13 22:46:31 +0530

    home page almost done

--- job-portal/src/components/Homepage.jsx
+++ job-portal/src/components/Homepage.jsx
@@ -12,31 +12,92 @@ function Homepage() {
 // const listItems = options.map((myList) =>
 // <li>{myList}</li> )
 return (
-<>
+<div className='homebody'>
 <div className='main'>
 <nav className='navbar'>
 <ul id='list1'>
 <li><a href='/regirecruiter'>Post Job</a></li>
 <li><a href='/login'>JobSeeker</a></li>
-<li><a href='/dashboard'>AboutUs</a></li>
-<li><a href='/dashboard'>ContactUs</a></li>
+<li><a href='/'>AboutUs</a></li>
+<li><a href='/'>ContactUs</a></li>
 </ul>
 <ul id='list2'>
-<li><a href='/dashboard'>MyAccount</a></li>
+<li><a href='/'>MyAccount</a></li>
 </ul>
 </nav>
 <div className='body'>
-<h3>Top Jobs</h3>
-<div className='jobcards'>
-<div className="card">
-<h3>CompanyName</h3>
-<h2>Job Title</h2>
-<h4>Job Description</h4>
+    <br></br>
+    <h2 className='jobshead'>Top Jobs </h2><br></br>
+    <div className='jobcards'>
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+        <div className="card">
+            <h3>CompanyName </h3>
+            <h2>Job Title</h2>
+            <h4>Job Description Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium a iusto consequuntur vitae, nesciunt dolorem corrupti, aliquid maiores aliquam, deleniti veniam. Aut modi molestias perspiciatis, voluptatum saepe magnam autem nulla!</h4>
+        </div>
+
+    </div>
+    <br></br>
+    <div className='newthings'>
+        <h2>Something New Is Coming Right There!!</h2>
+    </div>
+
+    
+</div>
+
+<div className="footer">
+        <h2>Footer</h2>
+</div>
+
 </div>
 </div>
-</div>
-</div>
-</>
 )
 }
 
