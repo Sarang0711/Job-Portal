d35abb35fa8c128ca1aec49ae70592cf468061c8
VaibhavDhaygude <vaibhavdhaygude70@gmail.com>
2023-04-18 22:49:27 +0530

    recruter registration db conncted

--- job-portal/src/components/Recrutergister.js
+++ job-portal/src/components/Recrutergister.js
@@ -55,16 +55,15 @@ function Registerrecruit(){
         <h2>Register as Recruter</h2>
         <form className="form">
                 <input type="text" placeholder="Your Name" name='recnamee'  id="0"></input>
-                <input type="text" placeholder="Company Name" name='reccomp'  id="1"></input>
-                <input type="text" placeholder="Your Roll at Company" name='recroll'  id="2"></input>
+                <input type="text" placeholder="Company Name" name='recnamee'  id="0"></input>
+                <input type="text" placeholder="Your Roll at Company" name='recnamee'  id="0"></input>
 
-                <input type="text" placeholder="Select a Username" name='username'  id="3"></input>
-                <input type="password" placeholder="Password" name='password'  id="4"></input>
+                <input type="text" placeholder="Select a Username" name='username'  id="1"></input>
+                <input type="password" placeholder="Password" name= 'password'  id="2"></input>
 
-                <input type="password" placeholder="Confirm your Password" name= 'password'  id="5"></input>
+                <input type="password" placeholder="Confirm your Password" name= 'password'  id="2"></input>
             </form>
             <button type="submit"   
-                onClick={addRecruter}
                 >
                    Register as Recruter 
                 </button>  
